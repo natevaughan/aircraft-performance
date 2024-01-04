210795854f8f75e851ba603be404655a66749009
Nate Vaughan <nate.vaughan@protonmail.com>
2024-01-04 12:21:09 -0600

    add serde, finish refactor of main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use aircraft_performance::Scaled;
+use aircraft_performance::Criteria;
 use crate::performance::calculate;
-use crate::performance::Criteria;
 
 mod performance;
 
@@ -20,66 +19,7 @@ fn main() {
         pressure_alt: pressure_alt,
         take_off_weight: take_off_weight,
         headwind: wind
-    })
+    });
     println!("Ground roll: {:>5}", performance.ground_roll);
     println!("Vr:          {:>5}", performance.vr);
-}
-
-fn search_for_nearest_curves<T: Scaled>(curves: &[T], scalar: f64) -> (&T, &T) {
-
-    if curves.len() < 2 {
-        panic!("Cannot interpolate fewer than 2 weight_curves")
-    }
-
-    let mut smallest = &curves[0];
-    let mut second_smallest = &curves[1];
-    for x in curves {
-        if (scalar - x.scalar()).abs() < (scalar - smallest.scalar()).abs() {
-            second_smallest = smallest;
-            smallest = &x;
-        }
-    }
-    (smallest, second_smallest)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const SMALLEST: QuadCurve = QuadCurve { scalar: -3.0, a: 1.0, b: 1.0, c: 1.0 };
-    const MIDDLE: QuadCurve = QuadCurve { scalar: -2.0, a: 1.0, b: 1.0, c: 1.0 };
-    const LARGEST: QuadCurve = QuadCurve { scalar: 4.0, a: 1.0, b: 1.0, c: 1.0 };
-    const X_LARGEST: QuadCurve = QuadCurve { scalar: 400.0, a: 1.0, b: 1.0, c: 1.0 };
-
-    #[test]
-    fn test_search_for_nearest_curves_order() {
-        let ordered = [SMALLEST, MIDDLE, LARGEST];
-        let (first, second) = search_for_nearest_curves(&ordered, -6.0);
-        assert_eq!(first, &SMALLEST);
-        assert_eq!(second, &MIDDLE);
-    }
-
-    #[test]
-    fn test_search_for_nearest_curves_two_in_wrong_order() {
-        let ordered = [LARGEST, SMALLEST];
-        let (first, second) = search_for_nearest_curves(&ordered, -6.0);
-        assert_eq!(first, &SMALLEST);
-        assert_eq!(second, &LARGEST);
-    }
-
-    #[test]
-    fn test_search_for_nearest_curves_unordered() {
-        let ordered = [MIDDLE, LARGEST, SMALLEST, X_LARGEST];
-        let (first, second) = search_for_nearest_curves(&ordered, -6.0);
-        assert_eq!(first, &SMALLEST);
-        assert_eq!(second, &MIDDLE);
-    }
-
-    #[test]
-    fn test_search_for_nearest_curves_reverse() {
-        let ordered = [X_LARGEST, LARGEST, MIDDLE, SMALLEST];
-        let (first, second) = search_for_nearest_curves(&ordered, -6.0);
-        assert_eq!(first, &SMALLEST);
-        assert_eq!(second, &MIDDLE);
-    }
 }
